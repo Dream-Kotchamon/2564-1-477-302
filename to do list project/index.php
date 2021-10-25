6a886ad95021f20470424ef09b0cde75c86535b0
Dream <dream.kotchamon@gmail.com>
2021-10-25 18:47:50 +0700

    .

--- to do list project/index.php
+++ to do list project/index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to To do List Project</h1>
 
     <?php if(isset($_GET['login'])) {echo "<h2>". $_GET['login'] .</h2>"; } ?>
-    
+
     <form action="con_login.php" method="post">
         <label>UserName: </label> <input type="text"> name="username"></input>
         <label>Password: </label> <input type="password"> name="password"></input>
